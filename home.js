8452635c319399ba63a80273d2262d76aa7f73dd
leleinc <674695840@qq.com>
2015-05-14 07:06:34 +0800

    home.js test

--- home.js
+++ home.js
@@ -1,4 +1,25 @@
-function fnt() {
-	console.log('http://www.zhihu.com/question/22146521');//home test
+// function fnt() {
+// 	var i = 0,j = 0;
+// 	console.log('http://www.zhihu.com/question/22146521');//home test
+// 	console.log(i++);
+// 	console.log(++j);
+// }
+// fnt();
+// var fs = require('fs');
+// function copy(src,dst){
+// 	fs.writeFileSync(dst,fs.readFileSync(src));
+// }
+// function main(argv){
+// 	copy(argv[0],argv[1]);
+// }
+// console.log(process.argv[0],process.argv[1]);
+// main(process.argv.slice(2));
+var fs = require('fs');
+function copy(src,dst){
+	fs.createReadStream(src).pipe(fs.createWriteStream(dst));
 }
-fnt();
+function main(argv){
+	copy(argv[0],argv[1]);
+}
+console.log("exe file path:"+process.argv[0],"main path:"+process.argv[1]);
+main(process.argv.slice(2));
